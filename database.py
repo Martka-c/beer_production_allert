4b396727b41d971ed52e9b3c96f98886f30c65b5
Martka <stiebler@poczta.fm>
2021-11-18 21:14:08 +0100

    Frontend cleaning, proper placement buttons and textboxes, added sending information to backend about allert that should be saved

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
+import datetime
 import sqlite3
 
 import mysql.connector
+
+from dictionary import RANGES, MESSAGES
 
 
 class InputDatabase:
@@ -12,19 +15,15 @@ class InputDatabase:
             password=password,
         )
         self.cursor = self.db.cursor(buffered=True)
+        self.start_creating_dbs_if_not_exist()
 
     def drop_db(self):
         query = "DROP DATABASE main_database"
         self.cursor.execute(query)
 
-    def start(self):
-        self.start_creating_dbs_if_not_exist()
-        self.fill_priority_table_if_not_filled()
-
     def start_creating_dbs_if_not_exist(self):
         self.create_db_if_not_exist()
         self.select_db()
-        self.drop_constant_tables()
         self.create_tables()
 
     def create_db_if_not_exist(self):
@@ -36,8 +35,6 @@ class InputDatabase:
 
     def create_tables(self):
         self.create_all_alarms_table()
-        self.create_alarm_types_table()
-        self.create_alarms_priority_table()
         self.create_users_table()
 
     def create_all_alarms_table(self):
@@ -47,69 +44,6 @@ class InputDatabase:
             self.cursor.execute(query)
         except sqlite3.OperationalError as ex:
             raise RuntimeError(f"Error during creating alarms table: {ex}")
-
-    def create_alarm_types_table(self):
-        """
-        Alarm ma obejmować wydarzenia, dzięki którym coś może sie zdupcyć
-        - przekroczenie temperatury przy ważeniu
-        - stłuczenie butelki (elementy szkła w urządzeniu
-        - nadmierne spienienie się płynu
-        - brak butelki pod podajnikiem
-        - brak cieczy w zbiorniku
-        - zapłon elementu
-        - odpadnięcie jakiegoś urządzenia
-        - przepalenie się żarówki alarmowej
-        """
-        try:
-            query = "CREATE TABLE IF NOT EXISTS alarm_types(id_type TEXT, type TEXT );"
-            self.cursor.execute(query)
-        except sqlite3.OperationalError as ex:
-            raise RuntimeError(f"Error during creating alarms_type table: {ex}")
-
-    def fill_alarm_types_table_if_not_filled(self):
-        """
-        Method checks if alarm types table contains any rows and if not - fill it.
-        :return: Doesn't return anything
-        """
-#        if self.check_if_table_is_empty("alarm_types"):
-        self.fill_alarm_types_table()
-
-    def fill_alarm_types_table(self):
-        try:
-            query = """INSERT INTO alarm_types(id_type, type) VALUES (1, 'Brak jęczmienia w maszynie do zacierania'), (2,'Usterka w młynie'),
-             (3, 'Usterka miksera do zacieru'), 
-             (4, 'Brak cieczy w kadzi do gotowania'), 
-             (5, 'Zbyt wysoka temperatura w zbiorniku do gotowania'), 
-             (6, 'Pęknięta kadź do gotowania'), 
-             (7, 'Uszkodzona uszczelka w chłodnicy (uszkodzona chłodnica)'),
-             (8, 'Nieszczelny zamknięty fermentator stożkowy'),
-             (9, 'W fermentatorze stożkowym znajduje się ciecz o zbyt wysokiej temperaturze (usterka chłodnicy)'),
-             (10, 'Uszkodzony zbiornik pośredni'),
-             (11, 'Pęknięta baryłka'),
-             (12, 'Uszkodzony filtr do oczyszczania piwa'),
-             (13, 'Zatkany filtr do oczyszczania piwa'),
-             (14, 'Pęknięty zbiornik oczyszczonego piwa'),
-             (15, 'Usterka pasteryzatora'),
-             (16, 'Zbyt wysoka temperatura podczas pasteryzacji'),
-             (17, 'Usterka maszyny pakującej'),
-             (18, 'Elementy szkła na taśmie produkcyjnej'),
-             """
-            self.cursor.execute(query)
-        except sqlite3.OperationalError as ex:
-            raise RuntimeError(f"Error during filling alarm types table: {ex}")
-
-    def create_alarms_priority_table(self):
-        """
-        Priorytety alarmów powinny obejmować:
-        - alarm krytyczny
-        - alarm o zagrożeniu
-        - alarm ostrzegawczy
-        """
-        try:
-            query = "CREATE TABLE IF NOT EXISTS priority_table(id_priority INTEGER(6), priority TEXT)"
-            self.cursor.execute(query)
-        except sqlite3.OperationalError as ex:
-            raise RuntimeError(f"Error during creating priority table: {ex}")
 
     def create_users_table(self):
         """
@@ -121,17 +55,6 @@ class InputDatabase:
         except sqlite3.OperationalError as ex:
             raise RuntimeError(f"Error during creating table users: {ex}")
 
-    def fill_priority_table_if_not_filled(self):
-        self.fill_priority_table()
-
-    def fill_priority_table(self):
-        try:
-            query = "INSERT INTO priority_table(id_priority, priority) VALUES (1, 'Normal'), (2,'Urgent'), (3, 'Critical')"
-            self.cursor.execute(query)
-            self.db.commit()
-        except sqlite3.OperationalError as ex:
-            raise RuntimeError(f"Error during filling priority_table: {ex}")
-
     def select_db(self):
         try:
             query = "USE main_database"
@@ -139,18 +62,15 @@ class InputDatabase:
         except sqlite3.OperationalError as ex:
             raise RuntimeError(f"Error: cannot use main_database\n{ex}")
 
-    def test_db(self):
-        query = "SELECT * FROM priority_table"
-        rows = self.cursor.execute(query).fetchall()
-        print([row[0] for row in rows])
-
-    def drop_constant_tables(self):
-        query = "DROP TABLE priority_table"
-        self.cursor.execute(query)
-        self.db.commit()
-
+    def raise_allert(self, allert_content: str, user: str):
+        try:
+            # datatime field format YYYY-MM-DD hh:mm:ss
+            query = f"INSERT INTO alarms(id, alert_type, alert_range, time, supervisor_name) " \
+                    f"VALUES (1, '{allert_content}', 'ok', '2021-11-18 23:37:00', {user})"
+            self.cursor.execute(query)
+            self.db.commit()
+        except sqlite3.OperationalError as ex:
+            raise RuntimeError(ex)
 
 if __name__ == '__main__':
     db = InputDatabase("root", "")
-    db.start()
-    #db.drop_db()
--- frontend.py
+++ frontend.py
@@ -3,83 +3,106 @@ import tkinter
 from tkinter import *
 
 # Create object
+import database
 from dictionary import USTERKI
-import env
+
 
 root = Tk()
+db = database.InputDatabase("root", "")
+user = "default"
+def raise_allert(incorrection_name: str, button_id: int = None):
+	if button_id not in [5, 9, 16]:
+		db.raise_allert(incorrection_name, user)
+	else:
+		db.raise_allert(USTERKI[button_id], user)
 
-def raise_allert(incorrection_name: str):
-	print(f"Temperature is too {incorrection_name}")
+
+def check_temperature_pasteryzator():
+	if temperatura_pasteryzator.get():
+		if int(temperatura_pasteryzator.get()) > 50:
+			raise_allert("Zbyt wysoka temperatura w pasteryzatorze")
+		elif int(temperatura_pasteryzator.get()) < 10:
+			raise_allert("Zbyt niska temperatura w pasteryzatorze")
 
 
-def check_temperature():
-	if int(temperatura.get()) > 50:
-		raise_allert("high")
-	elif int(temperatura.get()) < 10:
-		raise_allert("low")
+def check_temperature_fermentator():
+	if temperatura_w_fermentatorze.get():
+		if int(temperatura_w_fermentatorze.get()) > 50:
+			raise_allert("Zbyt wysoka temperatura w fermentatorze")
+		elif int(temperatura_w_fermentatorze.get()) < 10:
+			raise_allert("Zbyt niska temperatura w fermentatorze")
+
+
+def check_temperature_kadz():
+	if temperatura_zbiornik_gotowania.get():
+		if int(temperatura_zbiornik_gotowania.get()) > 50:
+			raise_allert("Zbyt wysoka temperatura w zbiorniku do gotowania")
+		elif int(temperatura_zbiornik_gotowania.get()) < 10:
+			raise_allert("Zbyt niska temperatura w zbiorniku do gotowania")
+
 
 # Adjust size
-root.geometry("1200x500")
+root.geometry("1200x800")
 
 # Add image file
 bg = PhotoImage(file=r"C:\Users\marta\Desktop\pivvo.png")
 
 # Create Canvas
-canvas1 = Canvas(root, width=400,
-				 height=400)
+canvas1 = Canvas(root, width=1200, height=800)
 
 canvas1.pack(fill="both", expand=True)
 
 # Display image
-canvas1.create_image(0, 0, image=bg,
-					 anchor="nw")
-
-# Add Text
-canvas1.create_text(200, 250, text="Welcome")
+canvas1.create_image(0, 0, image=bg, anchor="nw")
 
 # Create Buttons
-button1 = Button(root, text=USTERKI[1])
-button3 = Button(root, text=USTERKI[2])
-button2 = Button(root, text=USTERKI[3])
-button4 = Button(root, text=USTERKI[4])
-button5 = Button(root, text=USTERKI[5])
-button6 = Button(root, text=USTERKI[6])
-button7 = Button(root, text=USTERKI[7])
-button8 = Button(root, text=USTERKI[8])
-button9 = Button(root, text=USTERKI[9])
-button10 = Button(root, text=USTERKI[10])
-button11 = Button(root, text=USTERKI[11])
-button12 = Button(root, text=USTERKI[12])
-button13 = Button(root, text=USTERKI[13])
-button14 = Button(root, text=USTERKI[14])
-button15 = Button(root, text=USTERKI[15])
-button16 = Button(root, text=USTERKI[16], command=check_temperature)
-button17 = Button(root, text=USTERKI[17])
-button18 = Button(root, text=USTERKI[18])
+button1 = Button(root, text=USTERKI[1], command=lambda: raise_allert(USTERKI[1], 1))
+button2 = Button(root, text=USTERKI[2], command=lambda: raise_allert(USTERKI[2], 1))
+button3 = Button(root, text=USTERKI[3], command=lambda: raise_allert(USTERKI[3], 1))
+button4 = Button(root, text=USTERKI[4], command=lambda: raise_allert(USTERKI[4], 1))
+button5 = Button(root, text=USTERKI[5], command=check_temperature_kadz)
+button6 = Button(root, text=USTERKI[6], command=lambda: raise_allert(USTERKI[6], 1))
+button7 = Button(root, text=USTERKI[7], command=lambda: raise_allert(USTERKI[7], 1))
+button8 = Button(root, text=USTERKI[8], command=lambda: raise_allert(USTERKI[8], 1))
+button9 = Button(root, text=USTERKI[9], command=check_temperature_fermentator)
+button10 = Button(root, text=USTERKI[10], command=lambda: raise_allert(USTERKI[10], 1))
+button11 = Button(root, text=USTERKI[11], command=lambda: raise_allert(USTERKI[11], 1))
+button12 = Button(root, text=USTERKI[12], command=lambda: raise_allert(USTERKI[12], 1))
+button13 = Button(root, text=USTERKI[13], command=lambda: raise_allert(USTERKI[13], 1))
+button14 = Button(root, text=USTERKI[14], command=lambda: raise_allert(USTERKI[14], 1))
+button15 = Button(root, text=USTERKI[15], command=lambda: raise_allert(USTERKI[15], 1))
+button16 = Button(root, text=USTERKI[16], command=check_temperature_pasteryzator)
+button17 = Button(root, text=USTERKI[17], command=lambda: raise_allert(USTERKI[17], 1))
+button18 = Button(root, text=USTERKI[18], command=lambda: raise_allert(USTERKI[18], 1))
 
 # Display Buttons
-button1_canvas = canvas1.create_window(100, 10, anchor="nw", window=button1)
-button2_canvas = canvas1.create_window(100, 40, anchor="nw", window=button2)
-button3_canvas = canvas1.create_window(100, 70, anchor="nw", window=button3)
-button4_canvas = canvas1.create_window(100, 90, anchor="nw", window=button4)
-button5_canvas = canvas1.create_window(100, 110, anchor="nw", window=button5)
-button6_canvas = canvas1.create_window(100, 130, anchor="nw", window=button6)
-button7_canvas = canvas1.create_window(100, 150, anchor="nw", window=button7)
-button8_canvas = canvas1.create_window(100, 170, anchor="nw", window=button8)
-button9_canvas = canvas1.create_window(100, 190, anchor="nw", window=button9)
-button10_canvas = canvas1.create_window(100, 210, anchor="nw", window=button10)
-button11_canvas = canvas1.create_window(100, 230, anchor="nw", window=button11)
-button12_canvas = canvas1.create_window(100, 250, anchor="nw", window=button12)
-button13_canvas = canvas1.create_window(100, 270, anchor="nw", window=button13)
-button14_canvas = canvas1.create_window(100, 290, anchor="nw", window=button14)
-button15_canvas = canvas1.create_window(100, 310, anchor="nw", window=button15)
+button1_canvas = canvas1.create_window(10, 10, anchor="nw", window=button1) # brak jęczmienia
+button2_canvas = canvas1.create_window(120, 40, anchor="nw", window=button2) # usterka maszyny do zacieru
+button3_canvas = canvas1.create_window(300, 40, anchor="nw", window=button3) # usterka w młynie
+button4_canvas = canvas1.create_window(440, 230, anchor="nw", window=button4) #brak cieczy w kadzi do gotowania
+button5_canvas = canvas1.create_window(440, 300, anchor="nw", window=button5) #ustaw temperaturę w zbiorniku do gotowania
+button6_canvas = canvas1.create_window(470, 330, anchor="nw", window=button6) # pęknięta kadź gotowania
+button7_canvas = canvas1.create_window(110, 370, anchor="nw", window=button7)
+button8_canvas = canvas1.create_window(30, 420, anchor="nw", window=button8)
+button9_canvas = canvas1.create_window(20, 450, anchor="nw", window=button9)
+button10_canvas = canvas1.create_window(10, 560, anchor="nw", window=button10)
+button11_canvas = canvas1.create_window(10, 650, anchor="nw", window=button11)
+button12_canvas = canvas1.create_window(510, 500, anchor="nw", window=button12)
+button13_canvas = canvas1.create_window(530, 530, anchor="nw", window=button13)
+button14_canvas = canvas1.create_window(510, 560, anchor="nw", window=button14)
+button15_canvas = canvas1.create_window(480, 600, anchor="nw", window=button15)
+button16_canvas = canvas1.create_window(480, 660, anchor="nw", window=button16)
+button17_canvas = canvas1.create_window(250, 720, anchor="nw", window=button17)
+button18_canvas = canvas1.create_window(410, 720, anchor="nw", window=button18)
 
-temperatura = tkinter.Entry(root, state="normal")
-temp_canvas = canvas1.create_window(170, 330, anchor="nw", window=temperatura)
-button16_canvas = canvas1.create_window(100, 330, anchor="nw", window=button16)
+temperatura_pasteryzator = tkinter.Entry(root, state="normal")
+temp1_canvas = canvas1.create_window(480, 640, anchor="nw", window=temperatura_pasteryzator)
 
-button17_canvas = canvas1.create_window(100, 350, anchor="nw", window=button17)
-button18_canvas = canvas1.create_window(100, 370, anchor="nw", window=button18)
+temperatura_zbiornik_gotowania = tkinter.Entry(root, state="normal")
+temp2_canvas = canvas1.create_window(500, 280, anchor="nw", window=temperatura_zbiornik_gotowania)
+
+temperatura_w_fermentatorze = tkinter.Entry(root, state="normal") #temperatura w fermentatorze
+temp3_canvas = canvas1.create_window(10, 500, anchor="nw", window=temperatura_w_fermentatorze)
 
 
 # Execute tkinter
